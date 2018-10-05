587f6c562a0556e8ac0ab965db484c68fad21207
TaeHwan Park <eightynine01@mpaycheck.com>
2018-10-05 10:50:49 +0900

    자바스크립트 어노테이션 성공하고만다...

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { observer } from 'mobx-react';
+
+
+@observer
+class App extends React.Component {
+  componentDidMount(){}
+  render() {
+    return (
+      <div>
+        흠냐앙
+      </div>
+    );
+  }
+}
+
+export default App;
